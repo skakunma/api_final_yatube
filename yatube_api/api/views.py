14a863a4beb28ef30a55fc53b411aa5cf6d7963f
Дмитрий Скакун <skakun2025@mail.ru>
2024-07-26 04:03:24 +0300

    ss

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -38,9 +38,10 @@ class PostListCreateAPIView(generics.ListCreateAPIView):
     def list(self, request, *args, **kwargs):
         queryset = self.get_queryset()
 
-        if ('limit' not in request.query_params
-            and 'offset' not in request.query_params):
+        if ('limit' not in request.query_params 
+                and 'offset' not in request.query_params):
             serializer = self.get_serializer(queryset, many=True)
+
             return Response(serializer.data)
 
         paginator = self.pagination_class()
